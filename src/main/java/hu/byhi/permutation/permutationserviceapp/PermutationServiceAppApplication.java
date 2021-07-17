46c88c25cfff2c0333c4ddef1feb0567d17f02a1
byhi <byhisoft@gmail.com>
2021-07-17 16:45:11 +0200

    Init commit

---
+++ src/main/java/hu/byhi/permutation/permutationserviceapp/PermutationServiceAppApplication.java
@@ -0,0 +1,13 @@
+package hu.byhi.permutation.permutationserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermutationServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermutationServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/byhi/permutation/permutationserviceapp/PermutationServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.byhi.permutation.permutationserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermutationServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
